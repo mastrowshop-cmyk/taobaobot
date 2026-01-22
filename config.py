1c4610ea8df85f546d46accb4cffb18c8b649974
mastrowshop-cmyk <mastrowshop@gmail.com>
2026-01-23 08:56:51 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN = "8286881449:AAEx-whu3cmN_7erLeeqO4S0B4RuvGKPemg"
-ADMIN_ID = 7668402802  # твой Telegram ID
-WEBAPP_URL = "https://mastrowshop-cmyk.github.io/nataobaobot/"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_TG_ID = int(os.getenv("ADMIN_TG_ID"))
+WEBAPP_URL = os.getenv("WEBAPP_URL")
